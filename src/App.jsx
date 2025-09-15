44f8c87bfebaca2369939ae919f55ed625163446
Jack17kdb <jack@kali.Johnson.com>
2025-09-15 16:36:36 +0300

    full frontend development

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from 'react'
+import { FaSearch, FaTint, FaWind } from 'react-icons/fa'
+import sunny from './assets/sunny.png'
 
 function App() {
   return (
     <>
-      <h1>Hello, world!</h1>
+      <div className="form-div">
+        <div className="search-div">
+          <input className="search-bar" type="text" placeholder="Search"></input>
+          <FaSearch className="search-icon" />
+        </div>
+        <img src={sunny} alt="search" />
+        <div className="info">
+          <p className='temp'>61 °C</p>
+          <p className='location'>Nairobi</p>
+        </div>
+        <div className="details">
+          <div>
+            <div className="humid">
+              <FaTint /> 
+              <p>20%</p>
+            </div>
+            <span>humidity</span>
+          </div>
+          <div>
+            <div>
+              <FaWind />
+              <p>10 km/h</p>
+            </div>
+            <span>windspeed</span>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
